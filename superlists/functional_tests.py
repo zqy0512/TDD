b5211c6f09ec141666647805a3c8a9c37ca2d69c
zqy <1684010511@qq.com>
2022-04-19 10:57:12 +0800

    5.26

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -32,10 +32,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock features' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertTrue('1:Buy peacock features', [row.text for row in rows])
+        self.assertTrue('2:Use peacock feathers to make a fly', [row.text for row in rows])
         self.fail('Finish the test!')
 
 
