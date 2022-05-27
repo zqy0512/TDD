a41617ccc67a8a0717ed2d48867a8df9769eb634
zqy0512 <1684010511@qq.com>
2022-05-27 16:11:20 +0800

    the final

--- superlists/settings.py
+++ superlists/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [str.format(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
